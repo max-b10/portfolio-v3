c4ad41f8709a9e797d5e5bd5e329f568a4c80357
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-03 09:39:26 +0000

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primeflex/primeflex.css';
+
+import '../styles/_variables.css';
+import '../styles/globals.scss';
+
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=no,maximum-scale=1.0"
+        />
+        <title>Portfolio v3</title>
+        <meta name="description" content="Max Bungay's professional portfolio showcasing front end web development projects." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SWRConfig
+        value={{
+          shouldRetryOnError: false,
+          revalidateOnFocus: false,
+          revalidateIfStale: false,
+          revalidateOnMount: true,
+        }}
+     />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Home = (): JSX.Element => {
+
+  // Function to navigate to the /profile page
+ 
+  return (
+    <div>
+      <span>My new portfolio</span>
+    </div>
+  );
+};
+
+export default Home;
