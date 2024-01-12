2eebc57b266db9f78081c2780a32495d6c508449
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-12 10:22:58 +0000

    storybook automigration and other fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { Button } from "../UI/molecules/button/Button";
 
 const Home = (): JSX.Element => {
-  // Function to navigate to the /profile page
-
   return (
     <div>
       <span>My new portfolio</span>
-      <Button label="hi" />
+      <Button label={"hi"} />
     </div>
   );
 };
