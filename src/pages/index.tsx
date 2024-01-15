4c09f19585bf2134ffb364ea4ece66a0f6a07ba8
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-15 09:31:33 +0000

    rename and refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Button } from "../UI/molecules/button/Button";
-import Layout from "../app/components/Layout/Layout";
+import HeroSection from "../app/components/HeroSection/HeroSection";
 
 const Home = (): JSX.Element => {
   return (
-    <Layout>
-      <div className={`flex`}>
+    <>
+      <HeroSection />
+      <div className={`flex justify-content-center`}>
         <Button primary label={"Play Game"} />
         <Button primary label={"View Code"} />
       </div>
-    </Layout>
+    </>
   );
 };
 
