76a6bdc4bb136de71c0dde846e4371a91f107967
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-17 15:46:23 +0000

    structure and css

--- src/app/components/ProjectDisplay/ProjectDisplay.tsx
+++ src/app/components/ProjectDisplay/ProjectDisplay.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import styles from "./ProjectDisplay.module.scss";
 import Image from "next/image";
 import { Button } from "../../../UI/molecules/button/Button";
-
+import FlappyBirdPic from "../../../../public/images/ProjectPreviews/flappy-bird.png";
 interface ProjectDisplayProps {
   index: number;
   title: string;
-  imageSrc: string;
+  //   imageSrc: string;
   description: string;
 }
 
 const ProjectDisplay: React.FC<ProjectDisplayProps> = ({
   index,
   title,
-  imageSrc,
+  //   imageSrc,
   description,
 }) => {
   const isEven = index % 2 === 0;
@@ -24,13 +24,21 @@ const ProjectDisplay: React.FC<ProjectDisplayProps> = ({
         isEven ? styles.light : styles.dark
       }`}
     >
-      <div>
-        <h3>{title}</h3>
-      </div>
-      <div>
-        <Image src={imageSrc} alt="background image" height={150} width={150} />
-        <div>
-          <span>{description}</span>
+      <div className={styles.contentContainer}>
+        <div className={`${styles.projectHeader}`}>
+          <h3>{title}</h3>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={FlappyBirdPic}
+            className={styles.image}
+            alt="background image"
+            height={200}
+            width={200}
+          />
+        </div>
+        <div className={`${styles.projectDescription}`}>
+          <p>{description}</p>
         </div>
 
         <div className={`flex justify-content-center ${styles.test}`}>
