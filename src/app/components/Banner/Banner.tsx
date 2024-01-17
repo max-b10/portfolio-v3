f6303f15882bff4d42b029014337c6061d140e8a
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-17 11:14:17 +0000

    add border props

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./Banner.module.scss";
 import Image from "next/image";
+interface BannerProps {
+  showTopBorder?: boolean;
+  showBottomBorder?: boolean;
+}
 
-const Banner: React.FC = ({}) => {
+const Banner: React.FC<BannerProps> = ({
+  showTopBorder = false,
+  showBottomBorder = false,
+}) => {
   return (
-    <div className={`relative ${styles.banner}`}>
+    <div
+      className={`relative ${styles.banner} ${
+        showTopBorder ? styles.topBorder : ""
+      } ${showBottomBorder ? styles.bottomBorder : ""}`}
+    >
       <Image src={"/images/MB.png"} alt={"Mb"} width={16} height={16} />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Button } from "../UI/molecules/button/Button";
 import HeroSection from "../app/components/HeroSection/HeroSection";
-import Navbar from "../app/components/Navbar/Navbar";
+// import Navbar from "../app/components/Navbar/Navbar";
 import Banner from "../app/components/Banner/Banner";
 import styles from "./index.module.css";
 const Home = (): JSX.Element => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Banner showBottomBorder />
+
       <HeroSection />
-      <Banner />
+      <Banner showTopBorder showBottomBorder />
       <div className={`flex justify-content-center ${styles.test}`}>
         <Button primary label={"Play Game"} />
         <Button primary label={"View Code"} />
       </div>
+      <Banner showTopBorder />
     </>
   );
 };
