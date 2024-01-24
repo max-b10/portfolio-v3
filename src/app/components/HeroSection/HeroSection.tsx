1af93abaee6a7a55067f2e4c2bf0a3e88bdc893f
Max Bungay <maxbungay@maxs-mbp.home>
2024-01-24 15:27:08 +0000

    hero section progress

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -19,9 +19,24 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
         className={`absolute inset-0 flex justify-center items-center ${styles.overlay}`}
       >
         {/* <div>{title}</div> */}
-        <div className={styles.titleContainer}>
+        <div className={styles.headerContainer}>
           <div className={styles.name}>Max Bungay</div>
           <div className={styles.occupation}>Frontend Developer</div>
+          <div className={styles.nav}>
+            <nav>
+              <ol>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ol>
+            </nav>
+          </div>
         </div>
         <div className={styles.profileContainer}>
           <p className={styles.profileDescription}>
@@ -30,8 +45,8 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
             <span>portfolio,</span>
             <span>built using</span>
             <span>Next.js & React.</span>
-            <span>Feel free</span>
-            <span>to take a scroll</span>
+            <span>Feel free to </span>
+            <span>take a scroll</span>
             <span>down and check</span>
             <span>out some vanilla </span>
             <span>JavaScript Classics,</span>
